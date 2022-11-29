cd35450fe4c109b2babcbd738fcad3b4f9208be5
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-29 09:26:29 +0600

    create router

--- src/Route/Route/Route.js
+++ src/Route/Route/Route.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../../Pages/Home/Home/Home";
+import Main from "../../Pages/Layout/Main";
 
-const Route = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            // {
+            //     path: '/login',
+            //     element: <Login></Login>
+            // },
+            // {
+            //     path: '/signup',
+            //     element: <SIgnUp></SIgnUp>
+            // },
+            // {
+            //     path: '/appointment',
+            //     element: <Appointment></Appointment>
+            // }
+        ]
+    },
+   
+])
 
-export default Route;
+export default router;
