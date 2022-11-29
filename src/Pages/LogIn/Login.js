4502ac650d7480009337f3c5af4e4b90c957f7c4
ahasannahid <ahasan15-9072@diu.edu.bd>
2022-11-29 13:17:05 +0600

    add toast after successful login

--- src/Pages/LogIn/Login.js
+++ src/Pages/LogIn/Login.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from 'react';
 import { useForm } from "react-hook-form";
+import toast from 'react-hot-toast';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../Context/AuthProvider';
 
@@ -23,6 +24,7 @@ const Login = () => {
                 const user = result.user;
                 console.log(user);
                 setLoginUserEmail(data.email);
+                toast.success('User Login SUccessfully')
                 navigate(from, {replace: true})
             })
             .catch(error => {
